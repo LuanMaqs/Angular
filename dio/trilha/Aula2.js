11c6e71e8c1887e84c66ef64e8eb57009a4be863
Luan Marques <lm7007fernandes@gmail.com>
2023-03-23 22:13:24 -0300

    Conclusão de funções e entrada a objetos

--- dio/trilha/Aula2.js
+++ dio/trilha/Aula2.js
@@ -1,9 +1,34 @@
-function incrementoDeJuros(valor, percentualDeJuros) {
-    const valorAcrecimo = (percentualDeJuros / 100) * valor;
-    return valor + valorAcrecimo;
+function  incrementoDeJuros ( valor ,  percentualDeJuros )  {
+    const  valorAcrecimo  =  ( percentualDeJuros  /  100 )  *  valor ;
+    return  valor  +  valorAcrecimo ;
 
 }
 
 
-console.log(incrementoDeJuros(100, 10));
+console . log ( incrementoDeJuros ( 100,10 ) ) ;
 
+
+function meuNome(nome) {
+    nome = 'Luan'
+    console.log('Meu nome e: ' + nome);
+}
+
+meuNome();
+
+
+function minhaIdade(idade) {
+    if (idade < 18) {
+        console.log('Voce e menor de idade');
+
+    }else (idade > 18)
+    console.log('Voce e maior de idade');
+}
+
+minhaIdade(18);
+
+
+function calcularMedia(nota1, nota2) {
+    const media = (nota1 + nota2) / 2;
+}
+
+console.log(calcularMedia(5, 5));
