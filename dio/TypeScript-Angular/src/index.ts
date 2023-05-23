316760f4906956a9f9eab549219099d390190e0e
Luan Marques <lm7007fernandes@gmail.com>
2023-05-22 23:20:47 -0300

    Introduction TypeScript for Angular

--- dio/TypeScript-Angular/src/index.ts
+++ dio/TypeScript-Angular/src/index.ts
@@ -182,3 +182,42 @@ const stgArray = concatArray<string[]>(["Luan", "Goku"], ["Marcela"])
 console.log(numArray);
 console.log(stgArray)
 
+
+function apiVersion(version: string) {
+    return (target: any) => {
+      Object.assign(target.prototype, { __version: version, __name: "Luan" });
+    };
+  }
+  
+  //attribute decorator
+  function minLength(length: number) {
+    return (target: any, key: string) => {
+      let _value = target[key];
+  
+      const getter = () => "[play]" + _value;
+      const setter = (value: string) => {
+        if (value.length < length) {
+          throw new Error(`Tamanho menor do que ${length}`);
+        } else {
+          _value = value;
+        }
+      };
+  
+      Object.defineProperty(target, key, {
+        get: getter,
+        set: setter,
+      });
+    };
+  }
+  
+  class Api {
+    @minLength(10)
+    name: string;
+  
+    constructor(name: string) {
+      this.name = name;
+    }
+  }
+  
+  const api = new Api("usaadasdaaaaa");
+  console.log(api.name);
